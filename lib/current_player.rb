1c7f229b3bd18a7fb0f8f38ee2d9537503cf42c6
Claire Harmer <claire.f.harmer@gmail.com>
2018-06-22 16:29:17 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+ require 'pry'
+ 
+ board = ["X", " ", " ", "0", " ", " ", " ", "0", " "]
+ 
+ def turn_count(board)
+ board.each do |space|
+  counter = 0  
+  if space == "X" || space == "O" 
+    return #{counter}  
+  counter + 1
+ end
+end
+end
+  
